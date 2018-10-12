dba9187d0da899b782fde04db67764503455ce99
Radoslav Stefanov <radoslav.stefanov2000@gmail.com>
2018-10-12 14:41:06 +0200

    Update and rename printf_ascii.r to printf_ascii.s

--- printf_ascii.s
+++ printf_ascii.s
@@ -2,8 +2,8 @@
 
 main:
 
-	movq $0,%r11			
-	leal str,%esi			# effective address of string to %esi
+	movq $0,%r13			
+	leaq str,%rsi			# effective address of string to %esi
 
 	abc:
 
@@ -19,20 +19,28 @@ main:
 
 	lodsb				# read byte of string, then automaticaly increment position in string (next byte)
 
+
+
+	addq $-1, %rsi
 	movq %rax, %r12
+
+	cmpq $37,%r12			# if readed byte equal 0, so end of string reached
+	je   continue				
+	addq $1,%r13			# counter
+	
+
 	movq $1, %rax
 	movq $1, %rdi
 	movq $1, %rdx
  
 	syscall
 
+	addq $1,  %rsi
 	movq %rsi, %rax
 
-
-	cmpq $37,%r12			# if readed byte equal 0, so end of string reached
-	je   continue				
-	addq $1,%r11			# counter
 	jmp  abc			# repeat procedure
+
+	
 
 	continue:
 	//decq %r11
@@ -52,6 +60,6 @@ main:
 	call exit
 
 .data
-str: .asciz " This is some real %d decimal haha \n"	# if there is '\n', then it increment length by 1
+str: .asciz "This is some real %d decimal haha \n"	# if there is '\n', then it increment length by 1
 newline: .string " - "
 personalDetails: .asciz "\n %d \n"
