63b23d008b7402c0a7d76c774bbb81c2baa4bc40
Radoslav Stefanov <radoslav.stefanov2000@gmail.com>
2018-10-14 16:49:06 +0200

    Finish printf implementation

--- find%.s
+++ find%.s
@@ -1,40 +1,64 @@
 .data
-str: .asciz "My name is %s. I think I'll get a %u for my exam. What does %r do? And % \n"	# if there is '\n', then it increment length by 1
+str: .asciz "My name is %s. I think I'll get a %u for my exam. What does %d do? And % \n"	# if there is '\n', then it increment length by 1
 newline: .string " - "
 personalDetails: .asciz "\n %d \n"
-hello: .asciz "Herman \n"
+hello: .asciz "Herman"
+percentSign: .asciz "%"
+str2: .asciz "Hello %s %s %s %s %s %s %s \n"
 
+hello2: .asciz "Herman2"
+hello3: .asciz "Herman3"
+hello4: .asciz "Herman4"
+hello5: .asciz "Herman5"
+hello6: .asciz "Herman6"
+hello7: .asciz "Herman7"
 
 .globl main
 
+#include "printNumber.S"
+
 main:
+	movq $str2, %rdi
+	movq $hello, %rsi
+	movq $hello2, %rdx
+	movq $hello3, %rcx
+	movq $hello4, %r8
+	movq $hello5, %r9
+	push $hello6
+	push $hello7
+
+	call printf_hr
+
+
+	call exit
+printf_hr:
+	pop %r15
+	push %r9
+	push %r8
+	push %rcx
+	push %rdx
+	push %rsi
 
 	movq $0,%r13			
-	leaq str,%rsi			# effective address of string to %esi
+	movq %rdi,%rsi		# effective address of string to %esi
 
 	abc:
 
-	
-	syscall
-	// movq $1, %rax
-	// movq $1, %rdi
-	// movq $1, %rdx
-	// movq $newline , %rsi
-	// movq $3, %rdx
-
-	//syscall
 
 	movq $0, %rax
 
 	lodsb				# read byte of string, then automaticaly increment position in string (next byte)
 
-
-
 	addq $-1, %rsi
 	movq %rax, %r12
 
-	cmpq $37,%r12			# if readed byte equal 0, so end of string reached
-	je   continue				
+	cmpq $37,%r12
+	je   checkPercentSign	
+
+	// Check if end of string is reached, complete the subroutine
+	cmpq $0, %r12
+	je terminateReading
+
 	addq $1,%r13			# counter
 	
 
@@ -49,25 +73,46 @@ main:
 
 	jmp  abc			# repeat procedure
 
+	terminateReading:
+	ret
 	
 
-continue:
-	//decq %r11
+checkPercentSign:
 
-	lodsb				# read byte of string, then automaticaly increment position in string (next byte)
-	//addq $1, %rax
-	//movq %rax, %r14
 	
-
+	addq $1, %rsi
 	lodsb				# read byte of string, then automaticaly increment position in string (next byte)
-
 	movq %rax, %r14
 
 	//addq $-1, %rsi
 	//movq %rax, %r12
-	cmpq $115, %r14
+	//cmpq $115, %r14
 
-	je printshit
+
+	movq %r14, %rdi
+
+	cmpq $115, %r14
+	je printString
+
+	cmpq $117, %r14
+	je printDigit
+
+	cmpq $100, %r14
+	je printDigit
+
+	cmpq $32, %r14
+	je printPercent
+
+	call printNum
+
+
+	continuePercent: 
+	//addq $1, %rsi
+	movq %rsi, %rax
+
+	jmp abc
+
+	
 
 	//movq $0, %rax
 	//movq $personalDetails, %rdi
@@ -80,26 +125,53 @@ continue:
 	// movq $30, %rdx
  
 	// syscall
-	jmp abc
+	//jmp abc
 
-	call exit
-printshit:
-	movq $1, %rax
-	movq $1, %rdi
-	movq $hello , %rsi
-	movq $14, %rdx
- 	
+	//call exit
+
+printPercent:
+	movq $percentSign, %rdi
+	call printChar
+	jmp continuePercent
+
+printDigit:
+
+	pop %rdi
+	call printNum
+
+	jmp continuePercent
+
+printString:
+
+	pop %rax
+
+	movq %rsi, %r15
+
+	//push %rsi
+
+	movq %rax,%rsi
+	movq $0, %rax
+
+	readNextPartOfString:
+	lodsb
+
+	subq $1, %rsi
+
+	cmpq $0, %rax
+	je terminateStringPrinting
 
 
-	syscall
+	movq %rsi, %rdi
+	call printChar
+
+	addq $1, %rsi
+	jmp readNextPartOfString
 
 	//lodsb				# read byte of string, then automaticaly increment position in string (next byte)
-	addq $1, %rax
+	//addq $1, %rax
 
-
-	ret
+	terminateStringPrinting:
+	//pop %rsi
+	movq %r15, %rsi
+	jmp continuePercent
 	
-	
-
-
-
